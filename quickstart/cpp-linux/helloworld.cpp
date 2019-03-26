fb1e723649fe20fb2e9a07bfcf2d4c2c20874ce2
Amit Shukla <amishu@microsoft.com>
2019-03-26 16:39:07 -0700

    some changes for linux

--- quickstart/cpp-linux/helloworld.cpp
+++ quickstart/cpp-linux/helloworld.cpp
@@ -1,30 +1,73 @@
-//
-// Copyright (c) Microsoft. All rights reserved.
-// Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
-//
-
-// <code>
 #include <iostream> // cin, cout
 #include <speechapi_cxx.h>
 
 using namespace std;
 using namespace Microsoft::CognitiveServices::Speech;
+using namespace Microsoft::CognitiveServices::Speech::Audio;
 
-void recognizeSpeech() {
+std::shared_ptr<SpeechRecognizer> recognizer;
+std::shared_ptr<PullAudioInputStream> pullAudio;
+
+void* OpenCompressedFile(const std::string& compressedFileName)
+{
+    FILE *filep = NULL;
+    {
+#ifdef WIN32
+        std::wstring filename(compressedFileName.begin(), compressedFileName.end());
+        _wfopen_s(&filep, filename.c_str(), L"rb");
+#else
+        filep = fopen(compressedFileName.c_str(), "rb");
+#endif
+    }
+    return filep;
+}
+
+void closeStream(void* fp)
+{
+    fclose((FILE*)fp);
+}
+
+int ReadCompressedBinaryData(void *_stream, uint8_t *_ptr, uint32_t _buf_size)
+{
+    size_t  ret;
+    FILE* localRead = (FILE*)_stream;
+    ret = fread(_ptr, 1, _buf_size, localRead);
+    /*If ret==0 and !feof(stream), there was a read error.*/
+    return ret > 0 || feof(localRead) ? (int)ret : -128;
+}
+
+
+void recognizeSpeech(const std::string& subscriptionKey, const std::string& wavFileName) {
     // Creates an instance of a speech config with specified subscription key and service region.
     // Replace with your own subscription key and service region (e.g., "westus").
-    auto config = SpeechConfig::FromSubscription("YourSubscriptionKey", "YourServiceRegion");
+    auto config = SpeechConfig::FromSubscription(subscriptionKey, "westus");
 
-    // Creates a speech recognizer
-    auto recognizer = SpeechRecognizer::FromConfig(config);
-    cout << "Say something...\n";
+    
+    //if (wavFileName.find(".mp3") == (wavFileName.size() - 4))
+    //{
+    //    pullAudio = AudioInputStream::CreatePullStream(
+    //        AudioStreamFormat::GetCompressedFormat(AudioStreamContainerFormat::MP3),
+    //        OpenCompressedFile(wavFileName),
+    //        ReadCompressedBinaryData,
+    //        closeStream
+    //    );
+    //    recognizer = SpeechRecognizer::FromConfig(config, AudioConfig::FromStreamInput(m_pullAudioStream));
+    //}
+    //else if (wavFileName.find(".opus") == (wavFileName.size() - 5))
+    //{
+    //    pullAudio = AudioInputStream::CreatePullStream(
+    //        AudioStreamFormat::GetCompressedFormat(AudioStreamContainerFormat::OGG_OPUS),
+    //        OpenCompressedFile(wavFileName),
+    //        ReadCompressedBinaryData,
+    //        closeStream
+    //    );
+    //    recognizer = SpeechRecognizer::FromConfig(config, AudioConfig::FromStreamInput(m_pullAudioStream));
+    //}
+    //else
+    //{
+    recognizer = SpeechRecognizer::FromConfig(config, AudioConfig::FromWavFileInput(wavFileName));
+    //}
 
-    // Starts speech recognition, and returns after a single utterance is recognized. The end of a
-    // single utterance is determined by listening for silence at the end or until a maximum of 15
-    // seconds of audio is processed.  The task returns the recognition text as result. 
-    // Note: Since RecognizeOnceAsync() returns only a single utterance, it is suitable only for single
-    // shot recognition like command or query. 
-    // For long-running multi-utterance recognition, use StartContinuousRecognitionAsync() instead.
     auto result = recognizer->RecognizeOnceAsync().get();
 
     // Checks result.
@@ -48,7 +91,6 @@ void recognizeSpeech() {
 
 int main(int argc, char **argv) {
     setlocale(LC_ALL, "");
-    recognizeSpeech();
+    recognizeSpeech(argv[1], argv[2]);
     return 0;
 }
-// </code>
